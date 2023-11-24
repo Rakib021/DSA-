f7fe4cb8517640792497db7bc61508a445a3771a
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-11-24 20:11:54 +0600

    dsa

---
+++ ARRAY.py
@@ -0,0 +1,36 @@
+# list_arr =['alu','piaj','kumda','korola']
+
+# arr =[3,2,4,6,8,9,2,1]
+
+#print index 1 to 4
+# print(arr[start:end+1])
+
+# print(arr[1:5])
+
+
+# print(arr[-1])#slicing
+
+arr =[3,2,4,6,8,9,2,1]
+
+# print(max(arr))
+
+#raw code for max value
+
+# max_value = arr[0]
+
+# for value in arr:
+#     if value > max_value:
+#         max_value = value
+        
+# print("Raw code max value :",max_value)
+
+# print(min(arr))
+
+#raw code for minimum value
+
+min_value = arr[0]
+for value in arr:
+    if value < min_value:
+        min_value = value
+        
+print("raw code for minimum value:",min_value)
---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#Dictionary ={key:value}
+
+
+
+person ={
+    "name":"Rakibul Islam",
+    "age":21,
+    "grades" : {"math":50,'English':60,"bangla":33}
+    
+}
+
+print(person["grades"]["bangla"])
